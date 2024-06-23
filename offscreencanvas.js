9c548988cae71e3b3426053b05ed962a4acd85e6
Hatim <hatim.elar90@gmail.com>
2024-06-23 00:16:10 -0400

    Updates

--- offscreencanvas.js
+++ offscreencanvas.js
@@ -72,7 +72,24 @@ const COLORS = {
 function clamp(number, low, high) {
   return Math.max(Math.min(number, high), low);
 }
-function generateColorPalettes(imageData, maxPaletteSize) {
+function generateColorPalettes(imageBitmap, maxPaletteSize) {
+  const offscreenCanvas = new OffscreenCanvas(
+    imageBitmap.width,
+    imageBitmap.height
+  );
+  const context = offscreenCanvas.getContext("2d");
+
+  // Draw the ImageBitmap onto the OffscreenCanvas
+  context.drawImage(imageBitmap, 0, 0);
+
+  // Get ImageData from the OffscreenCanvas
+  const imageData = context.getImageData(
+    0,
+    0,
+    offscreenCanvas.width,
+    offscreenCanvas.height
+  );
+
   const palettes = [];
   postMessage({ stage: "Scanning image" });
   let colors = getUniqueColors(imageData);
